4ff972423a68cd14cf283a643dfb4c5a49f03b5a
pbcarloz <pb_carlos@outlook.com>
2022-05-16 22:41:54 -0500

    corregi el error de rutas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import Navigation from "./components/navigation/navigation.component";
 import Categorias from "./components/categories/itemList-container.jsx";
 
 import { Route, Routes } from "react-router-dom";
-import ItemDetailGorras from "./components/itemDetailContainer/itemDetailContainer.Gorras";
-import ItemDetailAbrigos from "./components/itemDetailContainer/itemDetailContainer.Abrigos";
 import ItemDetailCategory from "./components/itemDetailContainer/itemDetailContainer.Category";
-import ItemDetailTenis from "./components/itemDetailContainer/itemDetailContainer.tenis";
-import ItemDetailMujeres from "./components/itemDetailContainer/itemDetailContainer.Mujeres";
-import ItemDetailHombres from "./components/itemDetailContainer/itemDetailContainer.Hombres";
+
 import Carrito from "./components/carrito/carrito";
 
 
